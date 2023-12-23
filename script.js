6e67a48bad553a725a5725151b1599b6c7f0847a
Alicia Guo <axguo@mit.edu>
2023-12-22 21:51:19 -0600

    UI implementation

--- script.js
+++ script.js
@@ -1,31 +1,96 @@
+const entries = {};
 let currentDate = new Date();
 
-function updateJournalEntry() {
-    document.getElementById("date").innerText = currentDate.toDateString();
-    document.getElementById("entry-text").innerText = generateJournalEntry();
+function getEntryForDate(date) {
+
+    // Convert date to YYYY-MM-DD format
+    const dateKey = date.toISOString().slice(0, 10);
+
+    // Return saved entry if exists
+    if (entries[dateKey]) {
+        return entries[dateKey];
+    }
+
+    // Otherwise generate new entry
+    const entry = generateJournalEntry();
+
+    // Save it
+    entries[dateKey] = entry;
+
+    return entry;
 }
 
+
+function updateJournalEntry() {
+    const options = { weekday: 'long', month: 'short', day: 'numeric' };
+    document.getElementById("date").innerText = currentDate.toLocaleDateString('en-US', options);
+
+    const entry = getEntryForDate(currentDate);
+    document.getElementById("entry-text").innerText = entry;
+}
+
+updateJournalEntry(); // Initialize the first entry
+
+//////////////////////////
+// navigation buttons
+//////////////////////////
 document.getElementById("prev").addEventListener("click", () => {
-    console.log("click prev")
     currentDate.setDate(currentDate.getDate() - 1);
     updateJournalEntry();
 });
 
 document.getElementById("next").addEventListener("click", () => {
-    console.log("click next")
     currentDate.setDate(currentDate.getDate() + 1);
     updateJournalEntry();
 });
 
-updateJournalEntry(); // Initialize the first entry
+document.addEventListener("keydown", (e) => {
 
+    if (e.key === "ArrowLeft") {
+        currentDate.setDate(currentDate.getDate() - 1);
+        updateJournalEntry();
+    }
+
+    if (e.key === "ArrowRight") {
+        currentDate.setDate(currentDate.getDate() + 1);
+        updateJournalEntry();
+    }
+
+});
+
+
+//////////////////////////
+// generate entries
+//////////////////////////
 
 function generateJournalEntry() {
     const grammar = {
-        "start": "Today I $activity",
-        "activity": "went for a walk | read a book | cooked a meal | watched a movie"
+        "start": "$want",
+        "want": "I want to $dothing.",
+        "dothing": "write $a $poem",
+        "a": "a | a $adj",
+        "adj": RiTa.randomWord({ pos: "jj" }),
+        "poem": "poem | song | letter | verse | book"
     };
 
+
+    const loveGrammar = {
+        "start": "$question | $declaration | $wondering",
+        "question": "how do you know if $if ?",
+        "if": "you love somebody | it's right | they're the one | this is the way it's $sup to feel",
+        "sup": "supposed | meant",
+        "you": "you | I",
+    }
+
+
+
     const rg = RiTa.grammar(grammar);
+    const lg = RiTa.grammar(loveGrammar);
+
+
+
     return rg.expand();
 }
+
+
+// TODO: replace a with an
